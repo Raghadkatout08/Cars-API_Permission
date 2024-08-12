f154656d6431d815d71a4f688a150a34f0faac9c
raghadkatout08 <raghadkatout67@gmail.com>
2024-08-12 13:00:45 +0000

    deploy2

--- cars_store/settings.py
+++ cars_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pnz*$@=&^yxf#^d%l6m_+2am9l=pjwx=va^_xnh*shryq=b7h%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
