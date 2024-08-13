1accd716102b7c1833aaf0746015d4649f1c2287
raghadkatout08 <raghadkatout67@gmail.com>
2024-08-13 09:02:20 +0000

    lab34

--- cars_store/urls.py
+++ cars_store/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/cars_api/', include('cars_api.urls')),
-    path('', include('cars_api.urls')), 
+    path('', include('cars_api.urls')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
